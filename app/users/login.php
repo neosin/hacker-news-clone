65630f2a62363487c3c1f61b274847c115591cea
Simon Lindstedt <simon@Simons-MBP.lan>
2021-01-02 11:31:35 +0100

    small linting fix

--- app/users/login.php
+++ app/users/login.php
@@ -1,10 +1,10 @@
 <?php
 
 declare(strict_types=1);
+
 require __DIR__ . '/../autoload.php';
 
 if (isset($_POST['email'], $_POST['password'])) {
-
     $user = [
         "email" => $_POST['email'],
         "password" => $_POST['password']
