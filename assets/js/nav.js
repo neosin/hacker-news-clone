59a7c411d1ac48ff2a7309900dd57efff009171d
simonlindstedt <simon.lindstedt@gmail.com>
2021-01-13 08:16:11 +0100

    lint test again

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -2,23 +2,23 @@ const nav = document.querySelector("nav");
 const navItems = nav.querySelectorAll("li");
 
 navItems.forEach((navItem) => {
-   if (navItem.classList.contains("active")) {
-      navItem.classList.remove("active");
-   }
+  if (navItem.classList.contains("active")) {
+    navItem.classList.remove("active");
+  }
 });
 
 if (window.location.href.includes("top")) {
-   navItems[1].classList.toggle("active");
+  navItems[1].classList.toggle("active");
 } else if (window.location.href.includes("new")) {
-   navItems[2].classList.toggle("active");
+  navItems[2].classList.toggle("active");
 } else if (
-   window.location.href.includes("login") ||
-   window.location.href.includes("profile.php") ||
-   window.location.href.includes("edit.php")
+  window.location.href.includes("login") ||
+  window.location.href.includes("profile.php") ||
+  window.location.href.includes("edit.php")
 ) {
-   navItems[3].classList.toggle("active");
+  navItems[3].classList.toggle("active");
 } else if (window.location.href.includes("index.php")) {
-   navItems[0].classList.toggle("active");
+  navItems[0].classList.toggle("active");
 }
 
 // please fix
--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,15 +1,15 @@
 const forms = document.querySelectorAll("form");
 if (forms.length > 0) {
-   forms.forEach((form) => {
-      const deleteWarning = form.querySelector(".delete");
-      if (deleteWarning) {
-         deleteWarning.addEventListener("click", (e) => {
-            e.preventDefault();
-            let question = window.confirm("Are you sure you want to delete?");
-            if (question) {
-               form.submit();
-            }
-         });
-      }
-   });
+  forms.forEach((form) => {
+    const deleteWarning = form.querySelector(".delete");
+    if (deleteWarning) {
+      deleteWarning.addEventListener("click", (e) => {
+        e.preventDefault();
+        let question = window.confirm("Are you sure you want to delete?");
+        if (question) {
+          form.submit();
+        }
+      });
+    }
+  });
 }
